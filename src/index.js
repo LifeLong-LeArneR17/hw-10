a39bc67f8d1f1f851a720235d8b6cbbaec3db540
LifeLong-LeArneR17 <118177362+LifeLong-LeArneR17@users.noreply.github.com>
2023-03-14 22:45:51 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    formEl: document.querySelector("#search-box"),
+    cardEl: document.querySelector(".country-info"),
+}
+
+
+const onsearchCounry = event => {
+    const searchedQuery = event
+}
+
+
+refs.formEl.addEventListener('input', onsearchCounry)
